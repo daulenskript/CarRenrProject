9fc83fbf7e43897e27bb26858572771ffe774ce7
daulenskript <kazbekok7@gmail.com>
2024-09-13 01:28:57 +0600

    Admin class created

--- src/Admin.java
+++ src/Admin.java
@@ -1,2 +1,16 @@
-public class Admin {
+public class Admin extends User {
+    public Admin() {
+        super();
+    }
+
+    @Override
+    public void ShowList() {
+        System.out.println(" 1.Add new car");
+        System.out.println(" 2.Show all cars");
+        System.out.println(" 3.Update car");
+        System.out.println(" 4.Delete car");
+        System.out.println(" 5.Update new Admin");
+        System.out.println(" 6.Show rents");
+        System.out.println(" 7.Quit");
+    }
 }
